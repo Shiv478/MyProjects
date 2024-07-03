2d1b0c05fc20a711eedb707eed5a3eb8e5144540
Shiv478 <shiv.vinodshankar@gmail.com>
2024-07-02 22:47:28 -0700

    Tesst

--- LinkedListUnlimitedTimes/src/Main.java
+++ LinkedListUnlimitedTimes/src/Main.java
@@ -1,8 +1,9 @@
 import java.util.*;
 public class Main {
-    private ListNode head;
 
+    private ListNode head;
     private static class ListNode {
+
         private int data; //Generic Type
         private ListNode next;
 
@@ -10,52 +11,68 @@ public class Main {
             this.data = data;
             this.next = null;
         }
-        public static void main(String[] args){
-            Scanner input = new Scanner(System.in);
-            Main sll = new Main();
-            System.out.println("Please give me the number of elements you would like to add: ");
-            int rep = input.nextInt(); 
-            ListNode current = null; 
-            for(int i = 0; i<rep; i++){
-                if(i == 0) {
-                    System.out.println("What number are you adding: ");
+    }
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        Main sll = new Main();
+        int rep = 0;
+        System.out.println("Please give me the number of elements you would like to add: ");
+        if(input.hasNextInt()){
+            rep = input.nextInt();
+        }else{
+            System.out.println("Invalid Response");
+            System.exit(0);
+        }
+        ListNode current = null;
+        for(int i = 0; i<rep; i++){
+            if(i == 0) {
+                System.out.println("What number are you adding: ");
+                if(input.hasNextInt()) {
                     current = sll.head = new ListNode(input.nextInt());
                     System.out.println("1. " + current.data);
-                }else {
-                    System.out.println("What number are you adding: ");
-                    System.out.println(current);
+                }else{
+                    System.out.println("Invalid Response");
+                    System.exit(0);
+                }
+            }else {
+                System.out.println("What number are you adding: ");
+                System.out.println(current);
+                if(input.hasNextInt()) {
                     current.next = new ListNode(input.nextInt());
                     current = current.next;
-                    System.out.println("2. " +  current.data);
+                    System.out.println("2. " + current.data);
+                }else{
+                    System.out.println("Invalid Response");
                 }
-            }
-
-
-            int i = 0;
-            current = sll.head;
-
-
-            while(current != null){
-                System.out.print(current.data + " ");
-                current = current.next;
-            }
-            current = sll.head;
-            ListNode prev = null;
-            current = sll.head; //5
-            ListNode next = current.next; //10
-            current.next = prev;
-            while(next != null){
-                prev = current;
-                current = next;
-                next = current.next;
-                current.next = prev;
-            }
-            System.out.println();
-            while(current != null){
-                System.out.print(current.data + " ");
-                current = current.next;
             }
         }
 
+
+        int i = 0;
+        current = sll.head;
+
+
+        while(current != null){
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        current = sll.head;
+        ListNode prev = null;
+        current = sll.head; //5
+        ListNode next = current.next; //10
+        current.next = prev;
+        while(next != null){
+            prev = current;
+            current = next;
+            next = current.next;
+            current.next = prev;
+        }
+        System.out.println();
+        while(current != null){
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
     }
 }
---
+++ LinkedListUnlimitedTimes/src/TestCmd.java
@@ -0,0 +1,7 @@
+
+public class TestCmd
+{
+    public static void main(String[] args){
+        System.out.println("ok");
+    }
+}
